23711852fbed84412ec5c956f914872e5da41c56
Rui Catarino <55796280+ruitcatarino@users.noreply.github.com>
2024-07-20 21:35:34 +0100

    black

--- app/models/game.py
+++ app/models/game.py
@@ -6,11 +6,10 @@ class Game(Model):
     id = fields.IntField(pk=True)
     name = fields.CharField(max_length=255)
     created_at = fields.DatetimeField(auto_now_add=True)
-    user = fields.ForeignKeyField('models.User', related_name='games')
+    user = fields.ForeignKeyField("models.User", related_name="games")
 
     class Meta:
         table = "games"
 
     def __str__(self):
         return self.name
-    
