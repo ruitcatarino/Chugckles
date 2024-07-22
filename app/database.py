45b482b6154016c0ba26488212acab89014b9d05
Rui Catarino <55796280+ruitcatarino@users.noreply.github.com>
2024-07-22 02:00:26 +0100

    Add type hints

--- app/database.py
+++ app/database.py
@@ -16,17 +16,17 @@ TORTOISE_ORM = {
 }
 
 
-async def init_db():
+async def init_db() -> None:
     await Tortoise.init(config=TORTOISE_ORM)
     await Tortoise.generate_schemas()
     await prepopulate_db()
 
 
-async def close_db():
+async def close_db() -> None:
     await Tortoise.close_connections()
 
 
-async def prepopulate_db():
+async def prepopulate_db() -> None:
     if await Deck.all().count() == 0 and await Card.all().count() == 0:
         for deck_name, cards in POPULATE_DATA.items():
             new_deck = await Deck.create(name=deck_name)
