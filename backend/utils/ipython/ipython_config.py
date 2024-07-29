4f55006e98edd368d4fbca771d163753766e5fed
Rui Catarino <55796280+ruitcatarino@users.noreply.github.com>
2024-07-29 01:51:36 +0100

    Create ipython_config.py

---
+++ backend/utils/ipython/ipython_config.py
@@ -0,0 +1,22 @@
+c = get_config()
+
+c.TerminalIPythonApp.display_banner = True
+
+c.InteractiveShellApp.exec_lines = [
+    '%autoawait asyncio',
+]
+
+c.InteractiveShellApp.exec_files = [
+    'utils/ipython/startup.py'
+]
+
+c.InteractiveShell.autoindent = True
+c.InteractiveShell.confirm_exit = False
+c.InteractiveShell.deep_reload = True
+c.InteractiveShell.editor = 'nano'
+
+c.PrefilterManager.multi_line_specials = True
+
+c.AliasManager.user_aliases = [
+ ('la', 'ls -al')
+]
